a825c0234b9548d2d596dc96963c5316760e22ca
Oluyemi Paul <oluola96@gmail,com>
2022-10-12 17:44:50 +0100

    Feat: add image screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback } from 'react-native';
+import ViewImageScreen from './screen/ViewImageScreen';
 import WelcomeScreen from './screen/WelcomeScreen';
 
 const App = () => {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen  />
   )
 }
-
+ 
 export default App;
