a3312fb6633680242cb05e8aa289f96cba43d0b4
Oluyemi Paul <oluola96@gmail,com>
2022-10-12 16:47:31 +0100

    Feat: add welcome background image

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback } from 'react-native';
+import WelcomeScreen from './screen/WelcomeScreen';
 
-export default function App() {
- const handlePress =()=> {
-  return(
-    console.log("Text Clicked")
-  )
- }
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Open up App.js to start working on your app! reallly really long text</Text>
-     <Image 
-     blurRadius={30}
-     source={require("./assets/icon.png")}
-     style={styles.styleImage}/>
-    </SafeAreaView>
-  );
+    <WelcomeScreen />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#999',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  styleImage: {
-    width: 200,
-    height: 200,
-  }
-});
+export default App;
