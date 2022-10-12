d02f3c2826315028841c82d544e9b808563d4d19
Oluyemi Paul <oluola96@gmail,com>
2022-10-12 18:04:30 +0100

    Feat: add refactoring to color property

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback } from 'react-native';
-import ViewImageScreen from './screen/ViewImageScreen';
-import WelcomeScreen from './screen/WelcomeScreen';
+import ViewImageScreen from './app/screen/ViewImageScreen';
+import WelcomeScreen from './app/screen/WelcomeScreen';
 
 const App = () => {
   return (
